0088d8c36e47197f75d6abd79b73aef2c46de89b
Charles Njoku <hello@charlesnjoku.com>
2021-06-27 12:48:36 +0100

    'Privay' to 'Privacy'

--- src/components/Bv/Footer.vue
+++ src/components/Bv/Footer.vue
@@ -2,7 +2,7 @@
   <div class="footer">
     <div class="footercontent"><img src="images/pbyp.svg" loading="lazy" alt="Product by Pishure" class="image-7"></div>
     <div class="footerlabels">
-      <router-link :to="{ name: 'Privacy' }" class="link">Privay Policy</router-link>
+      <router-link :to="{ name: 'Privacy' }" class="link">Privacy Policy</router-link>
       <a href="mailto:hello@getpishure.com?subject=Bavatars%20Enquiry" class="link">Contact Us</a>
       <a href="#" class="link">Donate 💖</a>
     </div>
