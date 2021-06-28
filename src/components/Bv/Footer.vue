f511f8c683636ad4c724109279fef329ddcde18a
Charles Njoku <hello@charlesnjoku.com>
2021-06-28 16:22:40 +0100

    Add Product Hunt embed

--- src/components/Bv/Footer.vue
+++ src/components/Bv/Footer.vue
@@ -7,6 +7,8 @@
       <a href="https://useshukran.com/cr/pishure" class="link">Donate 💖</a>
     </div>
   </div>
+  <a href="https://www.producthunt.com/posts/bavatars-by-pishure?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-bavatars-by-pishure" target="_blank">
+  <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=300702&theme=light" alt="Bavatars by Pishure - Black Avatars for everyone, for free! | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
 </template>
 
 <script>
