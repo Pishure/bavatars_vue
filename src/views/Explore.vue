8ead9c1abe44869120f84d103274b9b2eb4bd9d9
MartinsOnuoha <martinsvictor.nd@gmail.com>
2021-06-28 04:24:54 +0100

    add download feature

--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -4,70 +4,19 @@
       <h1 class="heading-10">Bavatars - Black Avatars for free!</h1>
       <div class="bavatarssearch w-form">
         <form id="email-form" name="email-form" data-name="Email Form" class="form">
-          <div class="headerwrapper"><input type="text" class="bavatars_search w-input" maxlength="256" name="Bavatars-Search" data-name="Bavatars Search" placeholder="Search with keywords..." id="Bavatars-Search">
-            <a href="#" class="downloadbtn w-button">Download</a>
+          <div class="headerwrapper">
+            <input type="text" class="bavatars_search w-input" maxlength="256" name="Bavatars-Search" data-name="Bavatars Search" placeholder="Search with keywords..." id="Bavatars-Search">
+            <a @click="downloadImages" v-if="selectedItems.length" href="#" class="downloadbtn w-button">Download</a>
           </div>
           <div class="text-block-8"><span class="text-span-6">Available keywords:</span> Male, Female, Non-binary, Professional, Casual</div>
+          <div class="">Hi 👋🏼, Only One image can downloaded at a time, for now</div>
           <div id="bavatarsContainer" class="bavatarscontainer">
             <ul id="Bavatars" role="list" class="bavatarsitems w-list-unstyled">
-              <li class="bavataritem">
+              <li v-for="(item, i) in bavatars" :key="i" class="bavataritem">
                 <label class="w-checkbox">
-                  <div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input type="checkbox" id="Bavatar" name="Bavatar" data-name="Bavatar" style="opacity:0;position:absolute;z-index:-1"><span for="Bavatar" class="checkbox-label w-form-label">Checkbox</span>
+                  <div :style="`background: url(${item.url})`" class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input v-model="item.selected" type="checkbox" id="Bavatar" name="Bavatar" data-name="Bavatar" style="opacity:0;position:absolute;z-index:-1"><span for="Bavatar" class="checkbox-label w-form-label">Checkbox</span>
                 </label>
               </li>
-              <li class="bavataritem">
-                <label class="w-checkbox">
-                  <div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input type="checkbox" id="Bavatar-2" name="Bavatar-2" data-name="Bavatar 2" style="opacity:0;position:absolute;z-index:-1"><span for="Bavatar-2" class="checkbox-label w-form-label">Checkbox</span>
-                </label>
-              </li>
-              <li class="bavataritem">
-                <label class="w-checkbox">
-                  <div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input type="checkbox" id="Bavatar-3" name="Bavatar-3" data-name="Bavatar 3" style="opacity:0;position:absolute;z-index:-1"><span for="Bavatar-3" class="checkbox-label w-form-label">Checkbox</span>
-                </label>
-              </li>
-              <li class="bavataritem">
-                <label class="w-checkbox">
-                  <div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input type="checkbox" id="Bavatar-4" name="Bavatar-4" data-name="Bavatar 4" style="opacity:0;position:absolute;z-index:-1"><span for="Bavatar-4" class="checkbox-label w-form-label">Checkbox</span>
-                </label>
-              </li>
-              <li class="bavataritem">
-                <label class="w-checkbox">
-                  <div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input type="checkbox" id="Bavatar-5" name="Bavatar-5" data-name="Bavatar 5" style="opacity:0;position:absolute;z-index:-1"><span for="Bavatar-5" class="checkbox-label w-form-label">Checkbox</span>
-                </label>
-              </li>
-              <li class="bavataritem"><label class="w-checkbox">
-                  <div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input type="checkbox" id="Bavatar-6" name="Bavatar-6" data-name="Bavatar 6" style="opacity:0;position:absolute;z-index:-1"><span for="Bavatar-6" class="checkbox-label w-form-label">Checkbox</span>
-                </label></li>
-              <li class="bavataritem"><label class="w-checkbox">
-                  <div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input type="checkbox" id="Bavatar-7" name="Bavatar-7" data-name="Bavatar 7" style="opacity:0;position:absolute;z-index:-1"><span for="Bavatar-7" class="checkbox-label w-form-label">Checkbox</span>
-                </label></li>
-              <li class="bavataritem"><label class="w-checkbox">
-                  <div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input type="checkbox" id="Bavatar-8" name="Bavatar-8" data-name="Bavatar 8" style="opacity:0;position:absolute;z-index:-1"><span for="Bavatar-8" class="checkbox-label w-form-label">Checkbox</span>
-                </label></li>
-              <li class="bavataritem"><label class="w-checkbox">
-                  <div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input type="checkbox" id="Bavatar-9" name="Bavatar-9" data-name="Bavatar 9" style="opacity:0;position:absolute;z-index:-1"><span for="Bavatar-9" class="checkbox-label w-form-label">Checkbox</span>
-                </label></li>
-              <li class="bavataritem"><label class="w-checkbox">
-                  <div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input type="checkbox" id="Bavatar-10" name="Bavatar-10" data-name="Bavatar 10" style="opacity:0;position:absolute;z-index:-1"><span for="Bavatar-10" class="checkbox-label w-form-label">Checkbox</span>
-                </label></li>
-              <li class="bavataritem"><label class="w-checkbox">
-                  <div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input type="checkbox" id="Bavatar-11" name="Bavatar-11" data-name="Bavatar 11" style="opacity:0;position:absolute;z-index:-1"><span for="Bavatar-11" class="checkbox-label w-form-label">Checkbox</span>
-                </label></li>
-              <li class="bavataritem"><label class="w-checkbox">
-                  <div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input type="checkbox" id="Bavatar-12" name="Bavatar-12" data-name="Bavatar 12" style="opacity:0;position:absolute;z-index:-1"><span for="Bavatar-12" class="checkbox-label w-form-label">Checkbox</span>
-                </label></li>
-              <li class="bavataritem"><label class="w-checkbox">
-                  <div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input type="checkbox" id="Bavatar-13" name="Bavatar-13" data-name="Bavatar 13" style="opacity:0;position:absolute;z-index:-1"><span for="Bavatar-13" class="checkbox-label w-form-label">Checkbox</span>
-                </label></li>
-              <li class="bavataritem"><label class="w-checkbox">
-                  <div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input type="checkbox" id="Bavatar-14" name="Bavatar-14" data-name="Bavatar 14" style="opacity:0;position:absolute;z-index:-1"><span for="Bavatar-14" class="checkbox-label w-form-label">Checkbox</span>
-                </label></li>
-              <li class="bavataritem"><label class="w-checkbox">
-                  <div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input type="checkbox" id="Bavatar-15" name="Bavatar-15" data-name="Bavatar 15" style="opacity:0;position:absolute;z-index:-1"><span for="Bavatar-15" class="checkbox-label w-form-label">Checkbox</span>
-                </label></li>
-              <li class="bavataritem"><label class="w-checkbox">
-                  <div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input type="checkbox" id="Bavatar-16" name="Bavatar-16" data-name="Bavatar 16" style="opacity:0;position:absolute;z-index:-1"><span for="Bavatar-16" class="checkbox-label w-form-label">Checkbox</span>
-                </label></li>
             </ul>
           </div>
         </form>
@@ -82,8 +31,60 @@
   </div>
 </template>
 
+<style lang="scss">
+  .w-checkbox-input {
+    &--inputType-custom {
+      background-position: center !important;
+      background-repeat: no-repeat !important;
+      background-size: cover !important;
+    }
+  }
+</style>
 <script>
+import axios from 'axios';
+
 export default {
   name: 'explore',
+  data() {
+    return {
+      bavatars: [],
+      selectedItems: [],
+    };
+  },
+  watch: {
+    bavatars: {
+      deep: true,
+      handler() {
+        this.selectedItems = this.bavatars.filter((x) => x.selected);
+      },
+    },
+  },
+  methods: {
+    getBavatars() {
+      axios.get('https://bavatar.herokuapp.com/api/avatar')
+        .then((res) => {
+          this.bavatars = res.data.data;
+          console.log(res.data.data);
+        });
+    },
+    downloadImages() {
+      const item = this.selectedItems[0];
+      this.download(item.url, item.created_at);
+    },
+    toDataURL(url) {
+      return fetch(url).then((response) => response.blob()).then((blob) => URL.createObjectURL(blob));
+    },
+    async download(url, name) {
+      const a = document.createElement('a');
+      a.href = await this.toDataURL(url);
+      a.download = `${name}.png`;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    },
+  },
+  mounted() {
+    this.getBavatars();
+  },
 };
 </script>
