a9f86cea60113c6dc8da060d73a27e0de8c77e0c
Charles Njoku <hello@charlesnjoku.com>
2021-07-31 18:10:33 +0100

    Updates

--- src/components/Bv/Nav/Navbar.vue
+++ src/components/Bv/Nav/Navbar.vue
@@ -7,15 +7,7 @@
       </div>
       <nav role="navigation" class="nav-menu w-nav-menu">
         <router-link :to="{ name: 'Explore Bavatars' }" class="nav-link w-nav-link">Explore Bavatars</router-link>
-        <div data-hover="1" data-delay="0" class="nav-link w-dropdown">
-          <div class="w-dropdown-toggle" @click="showSubnav2">
-            <div class="icon w-icon-dropdown-toggle"></div>
-            <div class="text-block">Plugins</div>
-          </div>
-          <nav :style="`display: ${subnav2? 'block' : 'none'}; background: transparent`" id="subnav2" class="dropdown-list w-dropdown-list">
-            <router-link to="/" rel="noreferrer" href="https://www.figma.com/community/plugin/999661511334406897/Bavatars" class="dropdown-link w-dropdown-link">Figma</router-link>
-          </nav>
-        </div>
+        <router-link to="/" rel="noreferrer" href="https://www.figma.com/community/plugin/999661511334406897/Bavatars" target="_blank" class="nav-link w-nav-link">Figma Plugin</router-link>
         <router-link to="/" rel="noreferrer" href="https://api.bavatars.co/" class="nav-link w-nav-link">API</router-link>
         <div data-hover="1" data-delay="0" class="nav-link w-dropdown">
           <div class="w-dropdown-toggle" @click="showSubnav">
